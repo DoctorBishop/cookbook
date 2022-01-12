255fcc046fd71a0a123995b5f59f15fe3616f57d
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2022-01-12 16:01:36 +0200

    enum model binding

---
+++ CookBook/EnumsBinding/ModelBinding/EnumModelBinderProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace EnumsBinding.ModelBinding
+{
+    public class EnumModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (context.Metadata.IsEnumerableType)
+                return new EnumModelBinder();
+
+            return null;
+        }
+    }
+}
